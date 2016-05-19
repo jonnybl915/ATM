1b0c47009ef4ef593d36ada6a86c691d43260082
jonathandavidblack <jonnybl915@gmail.com>
2016-05-19 18:06:00 -0400

    added loops

--- src/com/theironyard/jdblack/Customer.java
+++ src/com/theironyard/jdblack/Customer.java
@@ -12,6 +12,7 @@ public class Customer {
     String choice;
 
     public void enterName() throws Exception {
+        System.out.println("Welcome to Wells Fargo!");
         System.out.println("What is your name?");
         String name = Main.scanner.nextLine(); //need to store it in a variable,
         System.out.println("Welcome, " + name);
@@ -22,30 +23,42 @@ public class Customer {
     }
 
     public void makeChoice() throws Exception {
-        System.out.println("What would you like to do today? [Check Balance/Withdraw Funds/Cancel]");
-        String choice = Main.scanner.nextLine();
+        System.out.println("What would you like to do today?");
+        int numInt = 2;
+        while (numInt <= 2) {
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw Funds");
+            System.out.println("3. Cancel");
+            String choice = Main.scanner.nextLine();
+            numInt = Integer.valueOf(choice);
+            if (1 == numInt) {
+                System.out.println(":/ your balance is $100.");
 
-        if (choice.equalsIgnoreCase("Check Balance")) {
-            System.out.println(":/ your balance is $100.");
-        }
+            } else if (numInt == 2) {
+                System.out.println("How much would you like to withdraw today?");
+                String amount = Main.scanner.nextLine();
 
-        else if (choice.equalsIgnoreCase("Cancel")) {
-            System.out.println("Thank you and please come again");
-        }
+                int numAmount = Integer.valueOf(amount); //converts to an integer
 
-        else if (choice.equalsIgnoreCase("Withdraw Funds")) {
-            System.out.println("How much would you like to withdraw today?");
-            String amount = Main.scanner.nextLine();
+                if (numAmount <= 100000 && numAmount >= 0)
+                    System.out.println("Please take your money, and have a great day!");
 
-            int numInt = Integer.valueOf(amount); //converts to an integer
-
-            if (numInt <= 100000 && numInt >= 0)
-                System.out.println("Please take your money, and have a great day!");
-
-            else {
-                throw new Exception("That is not a valid withdrawal amount");
+                else {
+                    throw new Exception("That is not a valid withdrawal amount");
+                }
             }
+            else if (numInt == 3) {
+                break;
+            }
+            System.out.println("Would you like to complete another transaction? [Y/N]");
+            String anotherChoice = Main.scanner.nextLine();
+            if (anotherChoice.equalsIgnoreCase("N")) {
+                break;
+            }
+
         }
+        System.out.println("Thank you and please come again");
     }
 }
+
 
