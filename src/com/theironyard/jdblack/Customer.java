54576bd81f76558e0be8c53f6fe158df0f8cc3e1
jonathandavidblack <jonnybl915@gmail.com>
2016-05-19 23:02:24 -0400

    added create account feature created hashmap in Customer class

--- src/com/theironyard/jdblack/Customer.java
+++ src/com/theironyard/jdblack/Customer.java
@@ -5,31 +5,47 @@ import com.theironyard.jdblack.Main;
 /**
  * Created by jonathandavidblack on 5/18/16.
  */
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Customer {
     String name;
     String choice;
+    HashMap<String, Double> customers;
 
     public void enterName() throws Exception {
         System.out.println("Welcome to Wells Fargo!");
         System.out.println("What is your name?");
-        String name = Main.scanner.nextLine(); //need to store it in a variable,
-        System.out.println("Welcome, " + name);
-
+        name = Main.scanner.nextLine(); //need to store it in a variable,
         if (name.isEmpty()) {
             throw new Exception("We do not recognize that name");
         }
+
+        if (customers.containsKey(name)){
+            System.out.println("Welcome, " + name);
+        }
+        else {
+            System.out.println("Name not recognized, would you like to open an account? [Y/N]");
+            String entry = Main.scanner.nextLine();
+            if (entry.equalsIgnoreCase("Y")){
+                System.out.println("Great!" + name + "Your account has been created.");
+                customers.put(name, 100.0);
+            }
+            else {
+                return;
+            }
+        }
+        this.makeChoice();
     }
 
     public void makeChoice() throws Exception {
         System.out.println("What would you like to do today?");
         int numInt = 2;
-        while (numInt <= 2) {
+        while (numInt != 3) {
             System.out.println("1. Check Balance");
             System.out.println("2. Withdraw Funds");
             System.out.println("3. Cancel");
-            String choice = Main.scanner.nextLine();
+            choice = Main.scanner.nextLine();
             numInt = Integer.valueOf(choice);
             if (1 == numInt) {
                 System.out.println(":/ your balance is $100.");
@@ -55,9 +71,17 @@ public class Customer {
             if (anotherChoice.equalsIgnoreCase("N")) {
                 break;
             }
-
         }
         System.out.println("Thank you and please come again");
+    }
+    public void makeAccount () {
+
+        customers = new HashMap<>();
+        customers.put("Hodor", 1000.0);
+        customers.put("John S", 2000.0);
+        customers.put("Sansa", 0.0);
+        customers.put("Bran", 1500.0);
+        customers.put("Brianne", 10000.0);
     }
 }
 
--- src/com/theironyard/jdblack/Main.java
+++ src/com/theironyard/jdblack/Main.java
@@ -9,18 +9,15 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     static Customer customer = new Customer();
     public static void main(String[] args) throws Exception {
+
+            customer.makeAccount();
+
         while(true) {
 
             customer.enterName();
-            customer.makeChoice();
+
         }
     }
 }
 
 
-//   HashMap<String, Double> customers = new HashMap<>();
-//        customers.put("Hodor", 1000.0);
-//        customers.put("John S", 2000.0);
-//        customers.put("Sansa", 0.0);
-//        customers.put("Bran", 1500.0);
-//        customers.put("Brianne", 10000.0);
